4df5662d47cf4b9bfbf600aaedeba9f91aae8301
Tom <twu@lcs.on.ca>
2023-05-23 14:29:14 -0400

    Initial Commit

---
+++ DaysSince/DaysSinceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaysSinceApp.swift
+//  DaysSince
+//
+//  Created by Tom Wu on 2023-05-23.
+//
+
+import SwiftUI
+
+@main
+struct DaysSinceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
